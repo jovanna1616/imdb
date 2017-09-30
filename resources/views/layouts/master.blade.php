5b0a4b8fc31e2b0abf6f9e00becdd3b6e0d68b66
jovanna <ladybutterfly.fly@gmail.com>
2017-09-30 21:46:51 +0200

    ukljucivanje bootstrap-a

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>IMBD</title>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+        <!-- Bootstrap CDN -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+
+    <body>
+        @include('partials.navigation')
+        <div class="blog-header">
+            <div class="container">
+                <h1 class="blog-title">@yield('title')</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 blog-main">
+                    @yield('movies')
+                </div><!-- /.col-sm-8 blog-main -->
+                @include('partials.sidebar')
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+        @include('partials.footer')
+    </body>
+</html>
---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('title')
+	ALL MOVIES
+@endsection
+
+@section('movies')
+	<div class="blog-post">
+        <h2 class="blog-post-title">Sample blog post</h2>
+        <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+        <hr>
+    </div><!-- /.blog-post -->
+@endsection
